c9f31a4673510aba643a9d33897878f106591704
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2023-05-23 20:44:16 +0200

    first commit

---
+++ Js/script.js
@@ -0,0 +1,496 @@
+/*
+DATA ----------------------------------------------
+*/
+const cards = [
+    {
+        "id" : 0,
+        "src" : "img/alternative_signs_small_arrow-0.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            },
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            },
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            },
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            },
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            },
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            },
+            {
+                "name" : "",
+                "date" : "",
+                "sign" : "",
+                "descr" : "",
+                "location" : ""
+            }
+        ]
+    },
+    {
+        "id" : 1,
+        "src" : "img/alternative_signs_small_arrow-1.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 2,
+        "src" : "img/alternative_signs_small_arrow-2.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 3,
+        "src" : "img/alternative_signs_small_arrow-3.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 4,
+        "src" : "img/alternative_signs_small_arrow-4.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 5,
+        "src" : "img/alternative_signs_small_arrow-5.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 6,
+        "src" : "img/alternative_signs_small_arrow-6.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 7,
+        "src" : "img/alternative_signs_small_arrow-7.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 8,
+        "src" : "img/alternative_signs_small_arrow-8.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 9,
+        "src" : "img/alternative_signs_small_arrow-9.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 10,
+        "src" : "img/alternative_signs_small_arrow-10.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 11,
+        "src" : "img/alternative_signs_small_arrow-11.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 12,
+        "src" : "img/alternative_signs_small_arrow-12.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 13,
+        "src" : "img/alternative_signs_small_arrow-13.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 14,
+        "src" : "img/alternative_signs_small_arrow-14.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 15,
+        "src" : "img/alternative_signs_small_arrow-15.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 16,
+        "src" : "img/alternative_signs_small_arrow-16.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 17,
+        "src" : "img/alternative_signs_small_arrow-17.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 18,
+        "src" : "img/alternative_signs_small_arrow-18.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    },
+    {
+        "id" : 19,
+        "src" : "img/alternative_signs_small_arrow-19.jpg",
+        "name" : "Name Img",
+        "text" : "text img lorem ipsum",
+        "messages" : [
+            {
+                "name" : "John Loz",
+                "date" : "18/05/2023",
+                "sign" : "Obbligo di sto gran cazzo",
+                "descr" : "Il cartello impone l'obbligo di sto gran cazzo, qualora l'obbligo non fosse chiaro sto cazzo ",
+                "location" : "Uscita verso sto grandissimo cazzo"
+            }
+        ]
+    }
+]
+
+let cnt = document.getElementById('mainContainer');
+let cardCnt = document.getElementById('cardContainer');
+let cardBox = document.getElementById('cardBox');
+let loadingScreen = document.querySelector('.loadingBg');
+let selectedCardId = 0;
+let selectCols = document.getElementById('selectCols');
+let infoBox;
+
+
+/*
+FUNCTIONS ----------------------------------------------
+*/
+function fillCnt (arrCards, container, cols) {
+
+    loading();
+
+    // svuoto il container in html
+    container.innerHTML = '';
+
+    // riempio il container con il contenuto selezionato
+    arrCards.forEach( (card) => {
+        container.innerHTML += `
+        <div class="card ${cols}" id="${card.id}" onclick="clickedCard(this.id)">
+            <img src="${card.src}" alt="">
+        </div>
+        `;
+    } );
+}
+
+function loading() {
+
+    // show loading ------------------------------------------------------
+    loadingScreen.classList.remove('hidden');
+    loadingScreen.classList.add('opacity'); 
+
+    // hide loading ------------------------------------------------------
+    setTimeout( ()=> {
+        loadingScreen.classList.remove('opacity'); 
+    }, 300)
+    setTimeout( ()=> {
+        loadingScreen.classList.add('hidden'); 
+    }, 400)
+}
+
+function clickedCard(id) {
+    loading();
+
+    selectedCardId = id;
+
+    cnt.classList.add("hidden");
+    selectCols.classList.add("hidden");
+    cardCnt.classList.remove("hidden");
+
+    let tempCard = {};
+
+    cards.forEach( (card) => {
+        if (card.id == id) {
+            tempCard = card;
+        }
+    } );
+
+    console.log('sono qui!')
+    console.log(tempCard)
+
+    cardBox.innerHTML = `
+    <div class="cardItem">
+        <img src="${tempCard.src}" alt="">
+    </div>
+    <div id="info${id}" class="cardInfo hidden"></div>
+    `;
+
+    infoBox = document.getElementById('info' + selectedCardId);
+
+    tempCard.messages.forEach( item => { console.log(infoBox)
+        infoBox.innerHTML += `
+        <div class="boxMsg">
+            <h3>${item.name}<h3/>
+            <p class="date">${item.date}<p/>
+            <p>${item.sign}<p/>
+            <p>${item.descr}<p/>
+            <p class="location">
+                <i class="fa-solid fa-location-dot"></i>
+                ${item.location}
+            <p/>
+        </div>
+        `;
+    });
+}
+
+function nextImg() {
+    if (selectedCardId == cards.length -1) {
+        selectedCardId = 0;
+    }
+    else {
+        selectedCardId++;
+    }
+    
+    clickedCard(selectedCardId);
+}
+
+function prevImg() {
+    if (selectedCardId == 0) {
+        selectedCardId = cards.length -1;
+    }
+    else {
+        selectedCardId--;
+    }
+
+    clickedCard(selectedCardId);
+}
+
+function showInfo() {
+    infoBox.classList.toggle('hidden')
+}
+
+function backToHome() {
+    loading();
+
+    cnt.classList.remove("hidden");
+    cardCnt.classList.add("hidden");
+    selectCols.classList.remove("hidden");
+}
+
+function changeView(n) {
+    switch(n) {
+        case 12:
+            fillCnt(cards, cnt, "cards_12");
+            break;
+        case 6: 
+            fillCnt(cards, cnt, "cards_6");       
+            break;
+        case 3:
+            fillCnt(cards, cnt, "cards_3");
+            break;
+    }
+} 
+
+function randomImg() {
+    let n = Math.floor(Math.random() * ((cards.length-1) - 0 + 1) + 0);
+    console.log(n)
+    clickedCard(n)
+}
+
+/*
+PROGRAM ----------------------------------------------
+*/  
+// fill main container with cards
+fillCnt(cards, cnt, "cards_12");
